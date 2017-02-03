70afdac5e57a789fa4f618694dd8e9d3696e00c3
Federico Gimenez <fgimenez@coit.es>
2017-02-03 10:22:56 +0100

    updated

---
+++ interfaces/mount/backend.go
@@ -0,0 +1,112 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2016-2017 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+// Package mount implements mounts that get mapped into the snap
+//
+// Snappy creates fstab like  configuration files that describe what
+// directories from the system or from other snaps should get mapped
+// into the snap.
+//
+// Each fstab like file looks like a regular fstab entry:
+//   /src/dir /dst/dir none bind 0 0
+//   /src/dir /dst/dir none bind,rw 0 0
+// but only bind mounts are supported
+package mount
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+
+	"github.com/snapcore/snapd/dirs"
+	"github.com/snapcore/snapd/interfaces"
+	"github.com/snapcore/snapd/osutil"
+	"github.com/snapcore/snapd/snap"
+)
+
+// Backend is responsible for maintaining mount files for snap-confine
+type Backend struct{}
+
+// Name returns the name of the backend.
+func (b *Backend) Name() interfaces.SecuritySystem {
+	return interfaces.SecurityMount
+}
+
+// Setup creates mount mount profile files specific to a given snap.
+func (b *Backend) Setup(snapInfo *snap.Info, confinement interfaces.ConfinementOptions, repo *interfaces.Repository) error {
+	// Collect all the mount snipptes
+	snapName := snapInfo.Name()
+	spec, err := repo.SnapSpecification(b.Name(), snapName)
+	if err != nil {
+		return fmt.Errorf("cannot obtain mount security snippets for snap %q: %s", snapName, err)
+	}
+	content := deriveContent(spec.(*Specification), snapInfo)
+	// synchronize the content with the filesystem
+	glob := fmt.Sprintf("%s.fstab", interfaces.SecurityTagGlob(snapName))
+	dir := dirs.SnapMountPolicyDir
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return fmt.Errorf("cannot create directory for mount configuration files %q: %s", dir, err)
+	}
+	if _, _, err := osutil.EnsureDirState(dir, glob, content); err != nil {
+		return fmt.Errorf("cannot synchronize mount configuration files for snap %q: %s", snapName, err)
+	}
+	return nil
+}
+
+// Remove removes mount configuration files of a given snap.
+//
+// This method should be called after removing a snap.
+func (b *Backend) Remove(snapName string) error {
+	glob := fmt.Sprintf("%s.fstab", interfaces.SecurityTagGlob(snapName))
+	_, _, err := osutil.EnsureDirState(dirs.SnapMountPolicyDir, glob, nil)
+	if err != nil {
+		return fmt.Errorf("cannot synchronize mount configuration files for snap %q: %s", snapName, err)
+	}
+	return nil
+}
+
+// deriveContent computes .fstab tables based on requests made to the specification.
+func deriveContent(spec *Specification, snapInfo *snap.Info) map[string]*osutil.FileState {
+	// No snippets? Nothing to do!
+	if len(spec.Snippets) == 0 {
+		return nil
+	}
+	// Compute the contents of the fstab file. It should contain all the mount
+	// rules collected by the backend controller.
+	var buffer bytes.Buffer
+	for _, snippet := range spec.Snippets {
+		fmt.Fprintf(&buffer, "%s\n", snippet)
+	}
+	fstate := &osutil.FileState{Content: buffer.Bytes(), Mode: 0644}
+	content := make(map[string]*osutil.FileState)
+	// Add legacy per-app/per-hook fstab files. Those are identical but
+	// snap-confine doesn't yet load it from a per-snap location. This can be
+	// safely removed once snap-confine is updated.
+	for _, appInfo := range snapInfo.Apps {
+		content[fmt.Sprintf("%s.fstab", appInfo.SecurityTag())] = fstate
+	}
+	for _, hookInfo := range snapInfo.Hooks {
+		content[fmt.Sprintf("%s.fstab", hookInfo.SecurityTag())] = fstate
+	}
+	return content
+}
+
+func (b *Backend) NewSpecification() interfaces.Specification {
+	return &Specification{}
+}
---
+++ interfaces/mount/backend_test.go
@@ -0,0 +1,165 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2016 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package mount_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+	"testing"
+
+	. "gopkg.in/check.v1"
+
+	"github.com/snapcore/snapd/dirs"
+	"github.com/snapcore/snapd/interfaces"
+	"github.com/snapcore/snapd/interfaces/ifacetest"
+	"github.com/snapcore/snapd/interfaces/mount"
+	"github.com/snapcore/snapd/osutil"
+)
+
+func Test(t *testing.T) {
+	TestingT(t)
+}
+
+type backendSuite struct {
+	ifacetest.BackendSuite
+
+	iface2 *ifacetest.TestInterface
+}
+
+var _ = Suite(&backendSuite{})
+
+func (s *backendSuite) SetUpTest(c *C) {
+	s.Backend = &mount.Backend{}
+	s.BackendSuite.SetUpTest(c)
+
+	c.Assert(s.Repo.AddBackend(s.Backend), IsNil)
+
+	err := os.MkdirAll(dirs.SnapMountPolicyDir, 0700)
+	c.Assert(err, IsNil)
+
+	// add second iface so that we actually test combining snippets
+	s.iface2 = &ifacetest.TestInterface{InterfaceName: "iface2"}
+	err = s.Repo.AddInterface(s.iface2)
+	c.Assert(err, IsNil)
+}
+
+func (s *backendSuite) TearDownTest(c *C) {
+	s.BackendSuite.TearDownTest(c)
+}
+
+func (s *backendSuite) TestName(c *C) {
+	c.Check(s.Backend.Name(), Equals, interfaces.SecurityMount)
+}
+
+func (s *backendSuite) TestRemove(c *C) {
+	appCanaryToGo := filepath.Join(dirs.SnapMountPolicyDir, "snap.hello-world.hello-world.fstab")
+	err := ioutil.WriteFile(appCanaryToGo, []byte("ni! ni! ni!"), 0644)
+	c.Assert(err, IsNil)
+
+	hookCanaryToGo := filepath.Join(dirs.SnapMountPolicyDir, "snap.hello-world.hook.configure.fstab")
+	err = ioutil.WriteFile(hookCanaryToGo, []byte("ni! ni! ni!"), 0644)
+	c.Assert(err, IsNil)
+
+	appCanaryToStay := filepath.Join(dirs.SnapMountPolicyDir, "snap.i-stay.really.fstab")
+	err = ioutil.WriteFile(appCanaryToStay, []byte("stay!"), 0644)
+	c.Assert(err, IsNil)
+
+	snapCanaryToStay := filepath.Join(dirs.SnapMountPolicyDir, "snap.i-stay.fstab")
+	err = ioutil.WriteFile(snapCanaryToStay, []byte("stay!"), 0644)
+	c.Assert(err, IsNil)
+
+	err = s.Backend.Remove("hello-world")
+	c.Assert(err, IsNil)
+
+	c.Assert(osutil.FileExists(appCanaryToGo), Equals, false)
+	c.Assert(osutil.FileExists(hookCanaryToGo), Equals, false)
+	content, err := ioutil.ReadFile(appCanaryToStay)
+	c.Assert(err, IsNil)
+	c.Assert(string(content), Equals, "stay!")
+	content, err = ioutil.ReadFile(snapCanaryToStay)
+	c.Assert(err, IsNil)
+	c.Assert(string(content), Equals, "stay!")
+}
+
+var mockSnapYaml = `name: snap-name
+version: 1
+apps:
+    app1:
+    app2:
+hooks:
+    configure:
+        plugs: [iface-plug]
+plugs:
+    iface-plug:
+        interface: iface
+slots:
+    iface-slot:
+        interface: iface2
+`
+
+func (s *backendSuite) TestSetupSetsupSimple(c *C) {
+	fsEntry1 := "/src-1 /dst-1 none bind,ro 0 0"
+	fsEntry2 := "/src-2 /dst-2 none bind,ro 0 0"
+
+	// Give the plug a permanent effect
+	s.Iface.MountPermanentPlugCallback = func(spec *mount.Specification, plug *interfaces.Plug) error {
+		return spec.AddSnippet(fsEntry1)
+	}
+	// Give the slot a permanent effect
+	s.iface2.MountPermanentSlotCallback = func(spec *mount.Specification, slot *interfaces.Slot) error {
+		return spec.AddSnippet(fsEntry2)
+	}
+
+	// confinement options are irrelevant to this security backend
+	s.InstallSnap(c, interfaces.ConfinementOptions{}, mockSnapYaml, 0)
+
+	// ensure both security effects from iface/iface2 are combined
+	// (because mount profiles are global in the whole snap)
+	expected := strings.Split(fmt.Sprintf("%s\n%s\n", fsEntry1, fsEntry2), "\n")
+	sort.Strings(expected)
+	// and that we have the legacy, per app/hook files as well.
+	for _, binary := range []string{"app1", "app2", "hook.configure"} {
+		fn := filepath.Join(dirs.SnapMountPolicyDir, fmt.Sprintf("snap.snap-name.%s.fstab", binary))
+		content, err := ioutil.ReadFile(fn)
+		c.Assert(err, IsNil, Commentf("Expected mount profile for %q", binary))
+		got := strings.Split(string(content), "\n")
+		sort.Strings(got)
+		c.Check(got, DeepEquals, expected)
+	}
+}
+
+func (s *backendSuite) TestSetupSetsupWithoutDir(c *C) {
+	s.Iface.MountPermanentPlugCallback = func(spec *mount.Specification, plug *interfaces.Plug) error {
+		return spec.AddSnippet("")
+	}
+
+	// Ensure that backend.Setup() creates the required dir on demand
+	os.Remove(dirs.SnapMountPolicyDir)
+	s.InstallSnap(c, interfaces.ConfinementOptions{}, mockSnapYaml, 0)
+
+	for _, binary := range []string{"app1", "app2", "hook.configure"} {
+		fn := filepath.Join(dirs.SnapMountPolicyDir, fmt.Sprintf("snap.snap-name.%s.fstab", binary))
+		c.Assert(osutil.FileExists(fn), Equals, true, Commentf("Expected mount file for %q", binary))
+	}
+}
